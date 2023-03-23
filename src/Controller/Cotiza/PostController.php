a081b86a376250ce20a271e86f3f96b2d67552a0
Ing. Aldo Cervantes <aldo.gdl@hotmail.com>
2023-03-23 08:58:31 -0600

    Test hacia SR

--- src/Controller/Cotiza/PostController.php
+++ src/Controller/Cotiza/PostController.php
@@ -15,7 +15,7 @@ use App\Repository\OrdenesRepository;
 use App\Repository\OrdenPiezasRepository;
 use App\Service\CotizaService;
 use App\Service\StatusRutas;
-
+use App\Service\WebHook;
 
 class PostController extends AbstractController
 {
@@ -45,6 +45,36 @@ class PostController extends AbstractController
     $data = $this->toArray($req, 'data');
     $contacsEm->safeTokenMessangings($data);
     return $this->json(['abort'=>false, 'msg' => 'ok', 'body' => []]);
+  }
+
+  /**
+   * Hacemos una prueba hacia el broker --front-door-- desde back-core
+   */
+  #[Route('cotiza/make-test/{token}/', methods:['post'])]
+  public function testToSistemNifi(Request $req, WebHook $wh, String $token): Response
+  {
+
+    $elToken = $this->getParameter('getAnToken');
+    if($elToken == $token) {
+
+      $data = $this->toArray($req, 'data');
+      if(array_key_exists('evento', $data)) {
+        $data['status'] = 'recibido';
+        // Enviamos el evento de nueva orden
+        $wh->sendMy(
+          $data, $this->getParameter('nifiFld'), $elToken
+        );      
+        return $this->json($data);
+      }
+    }
+
+    return $this->json([
+      'id'    => (string) round(microtime(true) * 1000),
+      'evento'=> 'test-fail',
+      'from'  => 'SR',
+      'to'    => 'Back-core',
+      'status'=> 'error',
+    ]);
   }
 
   #[Route('api/cotiza/set-orden/', methods:['post'])]
