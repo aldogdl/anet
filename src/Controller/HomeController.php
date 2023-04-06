34f9a0c80ee8c921679f7df565225fd13c464bd2
Ing. Aldo Cervantes <aldo.gdl@hotmail.com>
2023-04-05 20:06:46 -0600

    update url front door

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -27,4 +27,20 @@ class HomeController extends AbstractController
         }
         return $this->json(['abort'=> true, 'msg' => 'Mal-Bad', 'body' => 'Hola Intruso...']);
     }
+
+    #[Route('backcore/update-url-front-door/{pass}/{key}/', methods: ['GET'])]
+    public function updateUrlFrontDoor( string $pass, string $key ): Response
+    {
+        $res = base64_decode($key);
+        if($res == $this->getParameter('getAnToken')) {
+            $hash = file_put_contents(
+                '../front_door/front_door.txt/front_door.txt',
+                $pass
+            );
+            if($hash > 0) {
+                return $this->json(['abort'=> false, 'msg' => 'ok']);
+            }
+        }
+        return $this->json(['abort'=> true, 'msg' => 'Mal-Bad', 'body' => 'Hola Intruso...']);
+    }
 }
