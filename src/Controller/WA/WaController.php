16d567246c0d403c4383ec9538af4c1ddb39151d
Ing. Aldo Cervantes <aldo.gdl@hotmail.com>
2023-06-26 09:47:41 -0600

    me equiboque en la reparacion propuesta

--- src/Controller/WA/WaController.php
+++ src/Controller/WA/WaController.php
@@ -49,8 +49,8 @@ class WaController extends AbstractController
                 $filename = round(microtime(true) * 1000);
                 $path  = $pathTo.'/wa_'.$filename.'.json';
                 $pathPr= $pathTo.'/pr_'.$filename.'.json';
-                file_put_contents($pathPr, $has);
-                
+                file_put_contents($pathPr, json_encode($metadata->toArray()));
+
                 $metadata->pathToBackup = $path;
 
                 if($metadata->type != 'status' && $metadata->type != 'login') {
