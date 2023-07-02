887744094a0a9d659a20567770965da86cf5f1c3
Ing. Aldo Cervantes <aldo.gdl@hotmail.com>
2023-07-02 13:05:40 -0600

    Haciendo cambios que se me olvidaron hacer

--- src/Controller/MLM/MLMController.php
+++ src/Controller/MLM/MLMController.php
@@ -1,11 +1,7 @@
 <?php
 
-namespace App\Controller\WA;
+namespace App\Controller\MLM;
 
-use App\Service\WA\WaTypeResponse;
-use App\Service\WA\Dom\WaMessageDto;
-use App\Service\WA\WaService;
-use App\Service\WebHook;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
@@ -17,7 +13,7 @@ class MLMController extends AbstractController
      * Endpoint para la verificacion de mercado libre
      */
     #[Route('mlm/bienvenido/', methods: ['GET'])]
-    public function mlmWelcome(WebHook $wh, WaService $waS, Request $req): Response
+    public function mlmWelcome(Request $req): Response
     {
 
         if($req->getMethod() == 'GET') {
@@ -32,7 +28,7 @@ class MLMController extends AbstractController
      * Endpoint para recibir notificaciones
      */
     #[Route('mlm/notifications/', methods: ['POST'])]
-    public function mlmNotificaciones(WebHook $wh, WaService $waS, Request $req): Response
+    public function mlmNotificaciones(Request $req): Response
     {
 
         if($req->getMethod() == 'POST') {
@@ -49,16 +45,6 @@ class MLMController extends AbstractController
         }
 
         return $this->json( [], 500 );
-    }
-
-    /**
-     * Recuperamos el archivo de orden de llegada de los mensajes
-     */
-    #[Route('wa/get-orden-file/', methods: ['GET'])]
-    public function recoveryWaOrden(WaService $waS): Response
-    {
-        $res = $waS->getFileOrden($this->getParameter('waSort'));
-        return $this->json($res);
     }
 
 }
