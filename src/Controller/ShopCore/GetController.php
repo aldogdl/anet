5e4e03bd6d0c262229a973f0dcfd6a54079d3336
Ing. Aldo Cervantes <aldo.gdl@hotmail.com>
2023-07-26 11:32:29 -0600

    Probando otra respuesta

--- src/Controller/ShopCore/GetController.php
+++ src/Controller/ShopCore/GetController.php
@@ -22,10 +22,11 @@ class GetController extends AbstractController
     if($lock->isValid($token)) {
       $pathTo = $this->getParameter('dtaCtc') . $slug . '.json';
       if(is_file($pathTo)) {
-        $data = json_decode(file_get_contents($pathTo), true);
+        $data = file_get_contents($pathTo);
       }
     }
-    return $this->json($data);
+
+    return new Response($data);
   }
 
 }
