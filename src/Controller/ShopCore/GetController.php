3019820ec17b4f052cbfbb7c54de5cca34498435
Ing. Aldo Cervantes <aldo.gdl@hotmail.com>
2023-07-26 13:27:07 -0600

    reparando errores en json

--- src/Controller/ShopCore/GetController.php
+++ src/Controller/ShopCore/GetController.php
@@ -11,6 +11,7 @@ use App\Service\SecurityBasic;
 class GetController extends AbstractController
 {
 
+
   /** */
   #[Route('security-basic/get-data-ctc/{token}/{slug}/', methods:['get'])]
   public function getDataContact(
@@ -22,7 +23,11 @@ class GetController extends AbstractController
       $pathTo = $this->getParameter('dtaCtc') . $slug . '.json';
       if(is_file($pathTo)) {
         $data = file_get_contents($pathTo);
-        $data = json_encode($data);
+        $content = json_decode($data, true, 512, \JSON_BIGINT_AS_STRING | \JSON_THROW_ON_ERROR);
+        $content['lastEntrie'] = new \DateTime('now');
+        file_put_contents($pathTo, json_encode($content));
+        $data = file_get_contents($pathTo);
+        $data = json_decode($data, true, 512, \JSON_BIGINT_AS_STRING | \JSON_THROW_ON_ERROR);
       }
     }
 
