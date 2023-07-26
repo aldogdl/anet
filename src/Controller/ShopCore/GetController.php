483b6f4ff785059e45ef9255f69b4d80687fc07f
Ing. Aldo Cervantes <aldo.gdl@hotmail.com>
2023-07-26 13:56:34 -0600

    Creo que ya esta lo del decode

--- src/Controller/ShopCore/GetController.php
+++ src/Controller/ShopCore/GetController.php
@@ -23,10 +23,6 @@ class GetController extends AbstractController
       $pathTo = $this->getParameter('dtaCtc') . $slug . '.json';
       if(is_file($pathTo)) {
         $data = file_get_contents($pathTo);
-        $content = json_decode($data, true, 512, \JSON_BIGINT_AS_STRING | \JSON_THROW_ON_ERROR);
-        $content['lastEntrie'] = new \DateTime('now');
-        file_put_contents($pathTo, json_encode($content));
-        $data = file_get_contents($pathTo);
         $data = json_decode($data, true, 512, \JSON_BIGINT_AS_STRING | \JSON_THROW_ON_ERROR);
       }
     }
