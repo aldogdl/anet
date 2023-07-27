f35498cc205e7c46cd03df10edb8f856ff6c148f
Ing. Aldo Cervantes <aldo.gdl@hotmail.com>
2023-07-27 10:58:26 -0600

    creando el api para inv cot

--- src/Controller/ShopCore/GetController.php
+++ src/Controller/ShopCore/GetController.php
@@ -11,8 +11,9 @@ use App\Service\SecurityBasic;
 class GetController extends AbstractController
 {
 
-
-  /** */
+  /** 
+   * Recuperamos los datos del cotizador desde el archivo json
+  */
   #[Route('security-basic/get-data-ctc/{token}/{slug}/', methods:['get'])]
   public function getDataContact(
     SecurityBasic $lock, String $token, String $slug
@@ -23,7 +24,27 @@ class GetController extends AbstractController
       $pathTo = $this->getParameter('dtaCtc') . $slug . '.json';
       if(is_file($pathTo)) {
         $data = file_get_contents($pathTo);
-        $data = json_decode($data, true, 512, \JSON_BIGINT_AS_STRING | \JSON_THROW_ON_ERROR);
+        // $data = json_decode($data, true, 512, \JSON_BIGINT_AS_STRING | \JSON_THROW_ON_ERROR);
+      }
+    }
+
+    return $this->json($data);
+  }
+
+  /** 
+   * Recuperamos los datos del cotizador desde el archivo json
+  */
+  #[Route('security-basic/get-data-ctc/{token}/{slug}/', methods:['get'])]
+  public function getInvContact(
+    SecurityBasic $lock, String $token, String $waId
+  ): Response
+  {
+    $data = [];
+    if($lock->isValid($token)) {
+      $pathTo = $this->getParameter('invCtc') . $waId . '.json';
+      if(is_file($pathTo)) {
+        $data = file_get_contents($pathTo);
+        // $data = json_decode($data, true, 512, \JSON_BIGINT_AS_STRING | \JSON_THROW_ON_ERROR);
       }
     }
 
