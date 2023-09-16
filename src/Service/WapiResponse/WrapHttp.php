0e03c27755aea620080ee89253e0ae9c4328c36c
Ing. Aldo Cervantes <aldo.gdl@hotmail.com>
2023-09-15 19:11:11 -0600

    siguiendo error 2

--- src/Service/WapiResponse/WrapHttp.php
+++ src/Service/WapiResponse/WrapHttp.php
@@ -27,6 +27,9 @@ class WrapHttp
 
         if(count($this->bodyToSend) != 0) {
 
+            file_put_contents('prueba.json', json_encode([
+                'request' => $this->bodyToSend
+            ]));
             $response = $this->client->request(
                 'POST', $conm->uriBase.'/messages', [
                     'headers' => [
@@ -37,11 +40,7 @@ class WrapHttp
                 ]
             );
 
-            file_put_contents('prueba.json', json_encode([
-                'response' => $response->getContent(),
-                'request' => $this->bodyToSend
-            ]));
-            
+
             if($response->getStatusCode() == 200) {
                 return [];
             }
