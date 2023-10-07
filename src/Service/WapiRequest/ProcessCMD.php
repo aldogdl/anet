a5e7cce46eb2d0c67eb2db5ecd943bc4b8d2767d
Ing. Aldo Cervantes <aldo.gdl@hotmail.com>
2023-10-07 15:47:37 -0600

    sabe 2000

--- src/Service/WapiRequest/ProcessCMD.php
+++ src/Service/WapiRequest/ProcessCMD.php
@@ -34,8 +34,9 @@ class ProcessCMD {
     public function hasFileCmd(String $waId): bool
     {
         $this->waId = $waId;
-        $filename = trim($this->pathToCmd.$waId).'.json';
-        $exist = $this->filesystem->exists($filename);
+        $filename = trim($this->pathToCmd.$waId);
+        $filename = $filename.'.json';
+        $exist = ($this->filesystem->exists($filename)) ? true : false;
         file_put_contents('exit2_'.$exist.'_'.'.json', json_encode([
             'exit' => $exist, 'file' => $filename
         ]));
