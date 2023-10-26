98c2b4f1290fe12bc901130f6bc8393b3189b15f
Ing. Aldo Cervantes <aldo.gdl@hotmail.com>
2023-10-26 16:05:33 -0600

    Cambio body

--- src/Service/Mlm/MlmService.php
+++ src/Service/Mlm/MlmService.php
@@ -21,24 +21,23 @@ class MlmService
     public function send(): array
     {
         // 'body' => [
-        //     'grant_type' => 'authorization_code',
+        //     'grant_type' => 'refresh_token',
         //     'client_id'  => '3533349917060454',
         //     'client_secret' => 'hKnESsYNOP3QTqzhqFbKZL2eH3k0mMTt',
-        //     'code' => $this->codeAuth,
-        //     'redirect_uri' => 'https://autoparnet.com/mlm/code/',
-        //     'code_verifier' => 'shop2536core!1975s-b'
+        //     'refresh_token' => $this->codeAuth
         // ]
-
         $response = $this->client->request('POST', $this->urlMsgBase, [
             'headers' => [
                 'accept' => 'application/json',
                 'content-type' => 'application/x-www-form-urlencoded'
             ],
             'body' => [
-                'grant_type' => 'refresh_token',
+                'grant_type' => 'authorization_code',
                 'client_id'  => '3533349917060454',
                 'client_secret' => 'hKnESsYNOP3QTqzhqFbKZL2eH3k0mMTt',
-                'refresh_token' => $this->codeAuth
+                'code' => $this->codeAuth,
+                'redirect_uri' => 'https://autoparnet.com/mlm/code/',
+                'code_verifier' => 'shop2536core!1975s-b'
             ]
         ]);
 
