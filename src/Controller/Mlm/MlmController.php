9a4bed3528afa2b5c54f9ac3186f26622c6146dc
Ing. Aldo Cervantes <aldo.gdl@hotmail.com>
2023-10-27 23:11:06 -0600

    Modificaando ruta mlm

--- src/Controller/Mlm/MlmController.php
+++ src/Controller/Mlm/MlmController.php
@@ -14,13 +14,24 @@ class MlmController extends AbstractController
     /**
      * Endpoint para la verificacion de conección
      */
-    #[Route('mlm/code/', methods: ['GET', 'POST'])]
+    #[Route('mlm/code/', methods: ['GET'])]
     public function verifyMlm(Request $req): Response
     {
-        if($req->getMethod() == 'GET') {
-            return new Response();
+        $state = $req->query->get('state');
+        if(mb_strpos($state, ':') !== false) {
+            
+            $code = $req->query->get('code');
+            if(mb_strlen($code) > 10) {
+                $partes = explode(':', $state);
+                $action = $partes[0];
+                $slug = $partes[1];
+                file_put_contents('mlm_'.$slug.'.json', json_encode([
+                    'code' => $code, 'action' => $action, 'slug' => $slug
+                ]));
+                $this->redirect('https://www.autoparnet.com/shop/mlm-bind?state='.$state, 301);
+            }
         }
-        return new Response(500);
+        return new Response(200);
     }
 
     /**
@@ -38,22 +49,26 @@ class MlmController extends AbstractController
     }
 
     /**
+     * 
+     */
+    #[Route('mlm/get-code-auth/', methods: ['GET'])]
+    public function mlmGetCodeAuth(Request $req): Response
+    {
+        if($req->getMethod() == 'GET') {
+            $theGet = $this->getParameter('anetMlm');
+            $data = json_decode(file_get_contents($theGet), true);
+            return $this->json($data);
+        }
+        return new Response(500);
+    }
+
+    /**
      * Endpoint para la verificacion de conección
      */
     #[Route('mlm/code/notifications/', methods: ['GET', 'POST'])]
     public function notisMlm(): Response
     {
         return new Response('listo MLM');
-    }
-
-    /** */
-    #[Route('shop/mlm/', methods: ['get'])]
-    public function anulandoRoute(): RedirectResponse | Response
-    {
-    //   if($slug == '') {
-    //       return $this->json(['hola' => 'Bienvenido...']);
-    //   }
-      return $this->redirect('https://www.autoparnet.com/shop/?emp=iksan', 301);
     }
 
 }
