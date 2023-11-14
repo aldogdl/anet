057db0376b41bc9b5e6fc85a9ee65efaeabeb2a9
Ing. Aldo Cervantes <aldo.gdl@hotmail.com>
2023-11-13 22:42:46 -0600

    holita

--- src/Controller/ShopCore/PostController.php
+++ src/Controller/ShopCore/PostController.php
@@ -42,10 +42,8 @@ class PostController extends AbstractController
 	  return $this->json(['abort'=>false, 'msg' => 'ok', 'body' => ['nop' => 'nop']]);
 	}
 
-  /**
-   * api/
-  */
-  #[Route('shop-core/upload-img/', methods:['post'])]
+  /** */
+  #[Route('api/shop-core/upload-img/', methods:['post'])]
   public function uploadImg(Request $req, ShopCoreSystemFileService $sysFile): Response
   {
 
