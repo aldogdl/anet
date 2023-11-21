c311da080f026e119648f104a2394c396f270deb
Ing. Aldo Cervantes <aldo.gdl@hotmail.com>
2023-11-20 21:41:54 -0600

    sabe que estoy haciendo ya no pienso

--- public_html/index.php
+++ public_html/index.php
@@ -1,11 +1,18 @@
 <?php
 
-use App\Kernel;
-
-if ($_SERVER['REQUEST_METHOD'] == 'OPTIONS') {
+if ($_SERVER['REQUEST_METHOD'] === 'OPTIONS') {
+    header('Access-Control-Allow-Origin: *');
+    header('Access-Control-Allow-Methods: POST, GET, DELETE, PUT, PATCH, OPTIONS');
+    header('Access-Control-Allow-Headers: token, Content-Type');
+    header('Access-Control-Max-Age: 1728000');
+    header('Content-Length: 0');
+    header('Content-Type: text/plain');
     header('HTTP/1.1 200 OK');
-    exit();
+    die();
 }
+header('Access-Control-Allow-Origin: *');
+header('Content-Type: application/json');
+use App\Kernel;
 
 require_once dirname(__DIR__).'/vendor/autoload_runtime.php';
 
