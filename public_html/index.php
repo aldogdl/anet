3f1fae71cea364f83370d01f7188ebed725f718a
Ing. Aldo Cervantes <aldo.gdl@hotmail.com>
2023-11-21 10:23:33 -0600

    limpiando index

--- public_html/index.php
+++ public_html/index.php
@@ -1,17 +1,5 @@
 <?php
 
-if ($_SERVER['REQUEST_METHOD'] == 'OPTIONS') {
-    file_put_contents('cabecera.json', json_encode($_SERVER));
-    header('Access-Control-Allow-Origin: *');
-    header('Access-Control-Allow-Methods: POST, GET, DELETE, PUT, PATCH, OPTIONS');
-    header('Access-Control-Allow-Headers: token, Content-Type');
-    header('Access-Control-Max-Age: 1728000');
-    header('Content-Length: 0');
-    header('Content-Type: text/plain');
-    header('HTTP/1.1 200 OK');
-    die();
-}
-header('Access-Control-Allow-Origin: *');
 use App\Kernel;
 
 require_once dirname(__DIR__).'/vendor/autoload_runtime.php';
