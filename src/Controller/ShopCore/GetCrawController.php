411d25cafeec7fd77c0aea893e844209fcb6c297
Ing. Aldo Cervantes <aldo.gdl@hotmail.com>
2023-11-26 20:02:59 -0600

    probando protegindo

--- src/Controller/ShopCore/GetCrawController.php
+++ src/Controller/ShopCore/GetCrawController.php
@@ -2,33 +2,21 @@
 
 namespace App\Controller\ShopCore;
 
-use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-use Symfony\Component\HttpFoundation\Exception\JsonException;
-
-use App\Repository\AO1MarcasRepository;
-use App\Repository\AO2ModelosRepository;
-use App\Repository\NG2ContactosRepository;
-use App\Repository\ProductRepository;
 use App\Service\Crawlers\ToRadec;
-use App\Service\SecurityBasic;
-use App\Service\ShopCore\DataSimpleMlm;
-use App\Service\ShopCore\ShopCoreSystemFileService;
-use Symfony\Component\HttpFoundation\RedirectResponse;
 
 /**
  * Todos los get sin Token para la app de ShopCore
  */
 class GetCrawController extends AbstractController
 {
-  // https://www.radec.com.mx/catalogo?search=faro+audi+a3&op=Buscar
-  #[Route('shop/craw-rdec/{url}/', methods: ['get'])]
+  #[Route('api/shop/craw-rdec/{url}/', methods: ['get'])]
   public function getPage(String $url, ToRadec $craw): Response
   {
     if($url == '') {
-      return $this->json([]);
+      return new Response('', 200);
     }
     $html = $craw->load($url);
     return new Response($html, 200);
