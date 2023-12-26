f067a5e5bff936756eede5801cd2b141fbcffc2f
Ing. Aldo Cervantes <aldo.gdl@hotmail.com>
2023-12-26 10:14:08 -0600

    conmutador

--- src/Controller/EventCore/GetController.php
+++ src/Controller/EventCore/GetController.php
@@ -6,12 +6,33 @@ use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Exception\JsonException;
 
 use App\Repository\NG2ContactosRepository;
 use App\Service\AnetShop\AnetShopSystemFileService;
 
 class GetController extends AbstractController
 {
+
+    /**
+     * Obtenemos el request contenido decodificado como array
+     *
+     * @throws JsonException When the body cannot be decoded to an array
+     */
+    public function toArray(Request $req, String $campo): array
+    {
+        $content = $req->request->get($campo);
+        try {
+        $content = json_decode($content, true, 512, \JSON_BIGINT_AS_STRING | \JSON_THROW_ON_ERROR);
+        } catch (\JsonException $e) {
+        throw new JsonException(sprintf('No se puede decodificar el body, "%s".', get_debug_type($content)));
+        }
+
+        if (!\is_array($content)) {
+        throw new JsonException(sprintf('El contenido JSON esperaba un array, "%s" para retornar.', get_debug_type($content)));
+        }
+        return $content;
+    }
 
     #[Route('event-core/get-user-by-campo/', methods:['get'])]
     public function getUserByCampo(Request $req, NG2ContactosRepository $contacsEm): Response
@@ -58,6 +79,24 @@ class GetController extends AbstractController
                 unlink($filename);
                 return $this->json(['code' => 'exit']);
             }
+        }
+
+        return $this->json(['code' => 'error']);
+    }
+  
+    /** */
+    #[Route('event-core/data-conmutador/', methods: ['GET', 'POST'])]
+    public function getDataConmutador(Request $req): Response
+    {
+        $filename = 'tkwaconm.json';
+        if($req->getMethod() == 'GET') {
+            $data = file_get_contents($filename);
+            return $this->json(['code' => base64_encode($data)]);
+        }
+        
+        if($req->getMethod() == 'POST') {
+            $data = $this->toArray($req, 'data');
+            file_put_contents($filename, json_encode($data));
         }
 
         return $this->json(['code' => 'error']);
