55986459e6d05e64ab014a4b3c82f941f75ee8ea
Ing. Aldo Cervantes <aldo.gdl@hotmail.com>
2024-01-17 12:39:17 -0600

    pruba 1

--- src/Service/WapiProcess/CotTextProcess.php
+++ src/Service/WapiProcess/CotTextProcess.php
@@ -12,7 +12,7 @@ class CotTextProcess
     private array $cotProgress;
     private array $msgs = [
         'sdta' => ['current' => 'scto', 'next' => 'sgrx'],
-        'scto' => ['current' => 'sgrx', 'next' => 'sfto'],
+        'scto' => ['current' => 'sgrx', 'next' => 'sok'],
     ];
 
     /** */
@@ -32,31 +32,36 @@ class CotTextProcess
             return;
         }
         
-        // AQUI VOY hay un error al enviar el costo el cotProcess parece estar bacio...  0  
-
         // Actualizar el trackFile para el siguiente mensaje y contenido de cotizacion
         $this->cotProgress['current'] = $this->msgs[$current]['current'];
         $this->cotProgress['next']    = $this->msgs[$current]['next'];
         $this->cotProgress['track'][$campo] = $message->message;
 
         // Guardamos inmediatamente el cotProgess para evitar enviar los detalles nuevamente.
-        $fSys = new FsysProcess($paths['cotProgres']);
-        $fSys->setContent($message->from.'.json', $this->cotProgress);
+        $pathInit = ($this->cotProgress['current'] == 'sgrx') ? 'waTemplates' : 'cotProgres';
+        $fSys = new FsysProcess($pathInit);
+        if($pathInit == 'cotProgres') {
+            $fSys->setContent($message->from.'.json', $this->cotProgress);
+            $fSys->setPathBase($paths['waTemplates']);
+        }
 
         // Respondemos inmediatamente a este con el mensaje adecuado
-        $fSys->setPathBase($paths['waTemplates']);
         $template = $fSys->getContent($this->cotProgress['current'].'.json');
+        if(count($template) == 0) {
+            return;
+        }
         // Buscamos si contiene AnetLanguage para decodificar
         $deco = new DecodeTemplate($cotProgress);
         $template = $deco->decode($template);
         if(array_key_exists('wamid_cot', $this->cotProgress)) {
             $template['context'] = $this->cotProgress['wamid_cot'];
         }
-
         $sended = $this->sentMsg($template, $message, $wh, $wapiHttp, $paths['tkwaconm']);
+
         $recibido = $message->toArray();
         $fSys->setPathBase($paths['chat']);
         $fSys->dumpIn($recibido);
+
         if($this->entroToSended) {
             $fSys->dumpIn($sended);
         }
@@ -67,7 +72,7 @@ class CotTextProcess
             'trackfile'=> $this->cotProgress
         ]);
 
-        if($campo == 'precio') {
+        if($this->cotProgress['current'] == 'sgrx') {
             $this->fetchBait($message, $wh, $wapiHttp, $fSys, $paths);
         }
     }
@@ -81,35 +86,50 @@ class CotTextProcess
             'body' => $message->message
         ];
 
-        $tf = new TrackFileCot($message, $paths);
-        
-        $itemBaitToSent = $tf->lookForBait();
-        if(count($itemBaitToSent) > 0) {
+        $tf = new TrackFileCot($message, $paths, $fSys);
+        // Lo primero es eliminar del estanque la solicitud cotizada y enviarlo a tracked
+        $tf->finOfCotizacion();
+
+        $typeMsg = 'sgrx';
+        if(count($tf->cotProcess) > 0) {
 
             $this->cotProgress = $tf->cotProcess;
             //Buscamos para ver si existe el mensaje del item prefabricado.
             $tf->fSys->setPathBase($paths['prodTrack']);
-            $template = $tf->fSys->getContent($itemBaitToSent['idItem'].'_track.json');
-            
+            $template = $tf->fSys->getContent($this->cotProgress['idItem'].'_track.json');
             if(count($template) > 0) {
-
                 if(array_key_exists('message', $template)) {
+                    $typeMsg = 'sfto';
                     $template = $template['message'];
-                }
-                $this->entroToSended = false;
-                $sended = $this->sentMsg($template, $message, $wh, $wapiHttp, $paths['tkwaconm']);
-                $fSys->setPathBase($paths['chat']);
-                if($this->entroToSended) {
-                    $fSys->dumpIn($sended);
                 }
             }
         }
         
-        $wh->sendMy('wa-wh', 'notSave', [
-            'recibido' => ['type' => 'interactive', 'estanque' => 'fetch'],
-            'enviado'  => (count($sended) == 0) ? ['body' => 'none'] : $sended,
-            'trackfile'=> $this->cotProgress
-        ]);
+        if($typeMsg == 'sgrx') {
+            $this->cotProgress = [];
+            $tf->fSys->setPathBase($paths['waTemplates']);
+            $template = $tf->fSys->getContent($typeMsg.'.json');
+            if(count($template) == 0) {
+                return;
+            }
+        }
+
+        if(count($template) > 0) {
+
+            $this->entroToSended = false;
+            $sended = $this->sentMsg($template, $message, $wh, $wapiHttp, $paths['tkwaconm']);
+
+            $fSys->setPathBase($paths['chat']);
+            if($this->entroToSended) {
+                $fSys->dumpIn($sended);
+            }
+
+            $wh->sendMy('wa-wh', 'notSave', [
+                'recibido' => ['type' => 'interactive', 'estanque' => 'fetch'],
+                'enviado'  => (count($sended) == 0) ? ['body' => 'none'] : $sended,
+                'trackfile'=> $this->cotProgress
+            ]);
+        }
     }
 
     /** */
@@ -119,38 +139,32 @@ class CotTextProcess
 
         $typeMsgToSent = $template['type'];
         $conm = new ConmutadorWa($message->from, $pathCom);
-        if(count($template) > 0) {
-            
-            $conm->setBody($typeMsgToSent, $template);
-            $result = $wapiHttp->send($conm);
-            if($result['statuscode'] != 200) {
-                $this->entroToSended = false;
-                $wh->sendMy('wa-wh', 'notSave', $result);
-                return [];
-            }
-
-            $template = $template[$typeMsgToSent];
-            // Extraemos el IdItem del mensaje que se va a enviar al cotizador cuando se
-            // responde con otro mensaje interactivo
-            $idItem = '0';
-            if(array_key_exists('action', $template)) {
-                if(array_key_exists('buttons', $template['action'])) {
-                    $idItem = $template['action']['buttons'][0]['reply']['id'];
-                    $partes = explode('_', $idItem);
-                    $idItem = $partes[1];
-                }
-                $conm->bodyRaw = ['body' => $template['body'], 'idItem' => $idItem];
-            }else{
-                $conm->bodyRaw = $template['body'];
-            }
-
-            $objMdl = $conm->setIdToMsgSended($message, $result);
-            $conm = null;
-            $this->entroToSended = true;
-            return $objMdl->toArray();
+        $conm->setBody($typeMsgToSent, $template);
+        $result = $wapiHttp->send($conm);
+        if($result['statuscode'] != 200) {
+            $this->entroToSended = false;
+            $wh->sendMy('wa-wh', 'notSave', $result);
+            return [];
         }
 
+        $template = $template[$typeMsgToSent];
+        // Extraemos el IdItem del mensaje que se va a enviar al cotizador cuando se
+        // responde con otro mensaje interactivo
+        $idItem = '0';
+        if(array_key_exists('action', $template)) {
+            if(array_key_exists('buttons', $template['action'])) {
+                $idItem = $template['action']['buttons'][0]['reply']['id'];
+                $partes = explode('_', $idItem);
+                $idItem = $partes[1];
+            }
+            $conm->bodyRaw = ['body' => $template['body'], 'idItem' => $idItem];
+        }else{
+            $conm->bodyRaw = $template['body'];
+        }
+
+        $objMdl = $conm->setIdToMsgSended($message, $result);
         $conm = null;
-        return [];
+        $this->entroToSended = true;
+        return $objMdl->toArray();
     }
 }
--- src/Service/WapiProcess/TrackFileCot.php
+++ src/Service/WapiProcess/TrackFileCot.php
@@ -29,18 +29,33 @@ class TrackFileCot {
      * Buscamos el item respondido y lo pasamos a itemsToTrackeds.
      * A su ves lo eliminamos de la lista de trackFile
     */
-    public function __construct(WaMsgMdl $message, array $paths)
+    public function __construct(WaMsgMdl $message, array $paths, FsysProcess $fSys = null)
     {
         $this->paths      = $paths;
         $this->message    = $message;
         $this->isAtendido = false;
-        $this->fSys       = new FsysProcess($paths['trackeds']);
-        $trakeds          = $this->fSys->getContent($this->message->from.'.json');
+        if($fSys == null) {
+            $this->fSys = new FsysProcess($paths['trackeds']);
+            $this->getFileContentTrackeds();
+        }else{
+            $this->fSys = $fSys;
+            $this->fSys->setPathBase('trackeds');
+        }
+    }
+
+    /**
+     * Extraemos el archivo de rastreados del cotizador
+     */
+    public function getFileContentTrackeds(): array 
+    {
+        $trakeds = [];
+        $this->fSys->getContent($this->message->from.'.json');
         if(array_key_exists('idItem', $this->message->message)) {
             if(in_array($this->message->message['idItem'], $trakeds)) {
                 $this->isAtendido = true;
             }
         }
+        return $trakeds;
     }
 
     /**
@@ -66,24 +81,57 @@ class TrackFileCot {
         return false;
     }
 
+    /** */
+    public function finOfCotizacion(): void
+    {
+        $this->build();
+        
+        if(count($this->cotProcess) > 0) {
+            // Se encontró el item dentro del estanque
+            $trackeds = $this->getFileContentTrackeds();
+            if(!in_array($this->cotProcess['idItem'], $trackeds)) {
+                $trackeds[] = $this->cotProcess['idItem'];
+            }
+            $this->updateTrackeds($trackeds, false);
+
+            // El encontrado en el estanque lo borramos
+            unset($this->trackFile['items'][$this->indexItemTrigger]);
+            sort($this->trackFile['items']);
+            $this->updateTracking();
+
+            $this->cotProcess = [];
+            $bait = $this->lookForBait(true);
+            if(count($bait) > 0) {
+                $this->cotProcess = $bait;
+                $bait = [];
+            }
+        }
+    }
+
     /** 
     * Solo el no tengo, no tengo auto o fin de cotizacion, son los unicos eventos que
     * nos hace buscar una nueva carnada
     */
-    public function lookForBait(): array
+    public function lookForBait(bool $isBuilded = false): array
     {
-        $this->build();
-        $trackeds = [];
-        if($this->hasBaits) {
+        if(!$isBuilded) {
+            $this->build();
+        }
 
+        $itemFetchToSent = [];
+        if($this->hasBaits) {
+            
+            $trackeds = [];
             // En caso de que el Item se halla encontrado aun dentro de FileTrack lo enviamos
             // a trackeds y lo eliminamos del FileTrack
             $cotProcessIsFill = false;
+            $hasChangeFileTrack = false;
             if(count($this->cotProcess) > 0) {
                 if($this->trackFile['items'][$this->indexItemTrigger]['idItem'] == $this->cotProcess['idItem']) {
                     $trackeds[] = $this->cotProcess;
-                    unset($this->trackFile['items'][0]);
+                    unset($this->trackFile['items'][$this->indexItemTrigger]);
                     sort($this->trackFile['items']);
+                    $hasChangeFileTrack = true;
                 }
                 $cotProcessIsFill = true;
             }
@@ -103,13 +151,17 @@ class TrackFileCot {
                 }
                 sort($copyFileTrack);
                 $this->trackFile['items'] = $copyFileTrack;
+                $hasChangeFileTrack = true;
             }
 
             // Despues de haber echo la limpieza del trackFile, revisamos si quedan items
             if(count($this->trackFile['items']) > 0) {
                 $itemFetchToSent = $this->trackFile['items'][0];
             }
-            $this->updateTracking();
+
+            if($hasChangeFileTrack) {
+                $this->updateTracking();
+            }
 
             if(count($trackeds) > 0) {
                 $this->updateTrackeds($trackeds);
@@ -150,11 +202,15 @@ class TrackFileCot {
     }
 
     /** */
-    public function updateTrackeds(array $news)
+    public function updateTrackeds(array $news, bool $fetchInFile = true)
     {
         $this->fSys->setPathBase($this->paths['trackeds']);
-        $trackeds = $this->fSys->getTrackedsFileOf($this->message->from);
-        $trackeds = array_merge($trackeds, $news);
+        if($fetchInFile) {
+            $trackeds = $this->fSys->getTrackedsFileOf($this->message->from);
+            $trackeds = array_merge($trackeds, $news);
+        }else{
+            $trackeds = $news;
+        }
         $this->fSys->setContent($this->message->from.'.json', $trackeds);
     }
 
