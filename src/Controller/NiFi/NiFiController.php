68d7cd614b33f4d7e716e8de0bf77f3269655280
Ing. Aldo Cervantes <aldo.gdl@hotmail.com>
2024-05-07 17:12:30 -0600

    Eliminando todos

--- src/Controller/NiFi/NiFiController.php
+++ src/Controller/NiFi/NiFiController.php
@@ -2,8 +2,6 @@
 
 namespace App\Controller\NiFi;
 
-use App\Repository\OrdenesRepository;
-use App\Service\WebHook;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
@@ -19,52 +17,6 @@ class NiFiController extends AbstractController
     public function index(): Response
     {
         return $this->json(['hola' => 'Bienvenido...']);
-    }
-
-    /**
-     * 
-     */
-    #[Route('nifi/get-ordenes-ids/', methods: ['GET'])]
-    public function getOrdenesIds(OrdenesRepository $em): Response
-    {
-        $ids = [];
-        $result = $em->findAll();
-        if($result) {
-            foreach ($result as $orden) {
-                if(!in_array($orden->getId(), $ids)) {
-                    $ids[] = $orden->getId();
-                }
-            }
-        }
-        return $this->json(['abort'=> true, 'msg' => $ids]);
-    }
-
-    /**
-     * 
-     */
-    #[Route('nifi/orden/{id}/', methods: ['GET'])]
-    public function getOrden(WebHook $wh, OrdenesRepository $em, int $id): Response
-    {
-        $msg = 'No se encontró la orden con ID: '.$id;
-        $result = $em->find($id);
-        if($result) {
-            $toFile = $result->toArray();
-            $pathNifi = $this->getParameter('nifiFld');
-            $filename = $pathNifi.$id.'.json';
-            $payload = [
-                "evento" => "creada_solicitud",
-                "source" => $id.'.json'
-            ];
-
-            if($toFile) {
-                $content = file_put_contents($filename, json_encode($toFile));
-                if($content > 0) {
-                    $wh->sendMy('nifi\\orden', $filename, $toFile);
-                }
-            }
-            $msg = 'Guardada la Orden con el ID: '.$id;
-        }
-        return $this->json(['abort'=> true, 'msg' => $msg]);
     }
 
     /** */
