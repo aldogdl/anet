416584f4602b7f8e97463bd341716682bf286cf5
Ing. Aldo Cervantes <aldo.gdl@hotmail.com>
2024-05-11 11:54:22 -0600

    aver si asi

--- src/Service/AnetTrack/HcFotos.php
+++ src/Service/AnetTrack/HcFotos.php
@@ -44,8 +44,6 @@ class HcFotos
             }else {
                 // El usuario se arrepintio desea continuar con fotos
                 $this->bait['current'] = 'sfto';
-                $this->enviarMsg('sfto');
-                return;
             }
         }
 
