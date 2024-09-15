f392cdf825236dbe6c5c9e64dcabbb2395b7c959
Ing. Aldo Cervantes <aldo.gdl@hotmail.com>
2024-09-15 10:31:51 -0600

    dt cnx

--- src/Service/SecurityBasic.php
+++ src/Service/SecurityBasic.php
@@ -23,4 +23,27 @@ class SecurityBasic
         }
         return false;
     }
+
+    /** 
+     * Recuperamos los datos de conexion para la app de AnetCraw
+    */
+    public function getDtCnx(): array
+    {
+        $vers = $this->params->get('verapps');
+        $llave = $this->params->get('anetCnx');
+        $partes = explode(':', $llave);
+        $version = file_get_contents($vers);
+        $verAppShop = '';
+        $verAppCraw = '';
+        if($version != '') {
+            $version = json_decode($version, true);
+            $verAppShop (array_key_exists('anet_shop', $version)) ? $version['anet_shop'] : '';
+            $verAppCraw (array_key_exists('anet_craw', $version)) ? $version['anet_craw'] : '';
+        }
+        return [
+            'u' => $partes[0], 'p' => $partes[1],
+            'craw' => $verAppCraw, 'shop' => $verAppShop
+        ];
+    }
+
 }
