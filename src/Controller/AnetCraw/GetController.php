ef0b387d58c1f631b2f04990069fb1b501199708
Ing. Aldo Cervantes <aldo.gdl@hotmail.com>
2024-09-26 13:46:19 -0600

    probando otro metodo

--- src/Controller/AnetCraw/GetController.php
+++ src/Controller/AnetCraw/GetController.php
@@ -9,6 +9,7 @@ use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 use App\Service\SecurityBasic;
+use Symfony\Component\HttpFoundation\JsonResponse;
 
 /**
  * Todos los get sin Token para la app de AnetShop
@@ -25,11 +26,27 @@ class GetController extends AbstractController
     $lastTime = $req->query->get('last');
     $dql = $itemEm->getLastItems( $lastTime );
     $items = $dql->getArrayResult();
-    // Crear la respuesta JSON
-    $response = $this->json(['isOk' => true, 'msg' => 'ok', 'body' => $items]);
+    // // Crear la respuesta JSON
+    // $response = $this->json(['isOk' => true, 'msg' => 'ok', 'body' => $items]);
 
-    // Establecer el encabezado de compresión
+    // // Establecer el encabezado de compresión
+    // $response->headers->set('Content-Encoding', 'gzip');
+
+    $jsonContent = json_encode(['isOk' => true, 'msg' => 'ok', 'body' => $items]);
+
+    // Si json_encode falla, devuelve un error
+    if ($jsonContent === false) {
+        return new JsonResponse(['isOk' => false, 'msg' => 'Error al codificar JSON'], 500);
+    }
+
+    // Comienza el buffer de salida con Gzip
+    ob_start('ob_gzhandler');
+    echo $jsonContent; // Imprime el contenido JSON
+    $content = ob_get_clean(); // Obtiene el contenido comprimido
+
+    $response = new Response($content);
     $response->headers->set('Content-Encoding', 'gzip');
+    $response->headers->set('Content-Type', 'application/json'); // Asegúrate de establecer el tipo de contenido
 
     return $response;
   }
