417063902848ba3d375a884ad48721cc74a4a720
aldogdl <aldo.gdl@hotmail.com>
2025-06-29 08:50:50 -0600

    cambiando otra ves

--- src/Controller/Any/SysComController.php
+++ src/Controller/Any/SysComController.php
@@ -183,7 +183,7 @@ class SysComController extends AbstractController
     /** 
      * Validamos que el slug de la empresa este entre las registradas
     */
-    #[Route('/validate-nickname', methods: ['GET'])]
+    #[Route('/validate-nickname', methods: ['HEAD'])]
     public function validateNick(Request $req): Response
     {
         $data = $req->headers->get('x-nickname');
