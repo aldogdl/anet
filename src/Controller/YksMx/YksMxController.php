a06b1b0bc866239649e64834cc1c5f5f328703d4
aldogdl <aldo.gdl@hotmail.com>
2026-05-08 11:25:41 -0600

    esperando notof

--- src/Controller/YksMx/YksMxController.php
+++ src/Controller/YksMx/YksMxController.php
@@ -3,15 +3,19 @@
 namespace App\Controller\YksMx;
 
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 #[Route('/ynk-wh')]
 class YksMxController extends AbstractController
 {
-	#[Route('/', methods: ['get'])]
-	public function indexWh(): Response
+	#[Route('/', methods: ['get', 'post'])]
+	public function indexWh(Request $req): Response
 	{
+	  if($req->getMethod() == 'POST' ) {
+			file_put_contents('prueba_hook.json', json_encode($_POST));
+		}
 		return $this->json(['Yonkeros' => 'Bienvenido']);
 	}
 
